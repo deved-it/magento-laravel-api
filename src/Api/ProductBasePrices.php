7024d01ffa5e10b10d2bf1b515b165e75316477d
Salvatore Guarino <info@salgua.com>
2022-03-30 18:47:24 +0200

    DP

--- src/Api/ProductBasePrices.php
+++ src/Api/ProductBasePrices.php
@@ -2,6 +2,8 @@
 
 namespace Grayloon\Magento\Api;
 
+use App\Models\Price;
+
 class ProductBasePrices extends AbstractApi
 {
 
@@ -14,6 +16,18 @@ class ProductBasePrices extends AbstractApi
     {
         return $this->post('/products/base-prices-information', [
            'skus' => $skus
+        ]);
+    }
+
+    public function updatePrices(array $prices)
+    {
+        $priceArray = [];
+        /** @var Price $price */
+        foreach ($prices as $price) {
+            $priceArray[] = ['price' => $price->price, 'store_id' => $price->store_id, 'sku' => $price->product->sku];
+        }
+        return $this->post('/products/base-prices', [
+            'prices' => $priceArray
         ]);
     }
 }
